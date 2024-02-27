b522a7f7d4c2c84e8c93adcd365553b1919158e9
Diego-Barreto-web <diegobarretopro@gmail.com>
2024-02-27 10:31:16 -0300

    FakePinterest v0.9

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = '4e1eec4b40ae624a3529ff11736066ec'
+app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
